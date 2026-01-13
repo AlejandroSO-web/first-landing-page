d31f6f5ff512e755f4252bb56bba955773ce6f23
Alejandro Sánchez <78751248+AlejandroSO-web@users.noreply.github.com>
2026-01-13 19:35:50 +0100

    Añadir hora exacta a nuestro pedido.
    Añadir hora exacta a nuestro pedido.

--- CafeArtesano/script.js
+++ CafeArtesano/script.js
@@ -5,10 +5,17 @@ const titulo = document.querySelector('h1');
 /*Aplicamos accion*/
 
 botonPedido.addEventListener('click',function(){
+    const ahora = new Date();
+    const hora = ahora.getHours();
+    const minutos = ahora.getMinutes().toString().padStart(2,'0'); /*Para que salga 09 y no 9*/
+    const tiempo = hora + ":" + minutos;
     titulo.innerHTML= "¡Marchando tu cafe!";
     titulo.style.color = "#d4a373";
-    botonPedido.innerText ="En camino...";
+    const mensajeHora = document.createElement('p');
+    mensajeHora.innerText = 'Pedido realizado a las ' + tiempo;
+    mensajeHora.style.marginTop = "15px";
+    mensajeHora.style.fontWeight = "bold";
+    document.querySelector('.hero').appendChild(mensajeHora);  
     botonPedido.disabled = true;
-    botonPedido.style.opacity  ="0.7";
-    botonPedido.style.cursor = "not-allowed";
+    botonPedido.innerText ="En camino... ";
 });
