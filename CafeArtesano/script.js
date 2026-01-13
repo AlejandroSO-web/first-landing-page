d994a467342c6a8469b43a13d711366b3d8628b6
Alejandro Sánchez <78751248+AlejandroSO-web@users.noreply.github.com>
2026-01-13 21:37:44 +0100

    Implementar nueva lista con JavaScript
    Implementar nueva lista con JavaScript

--- CafeArtesano/script.js
+++ CafeArtesano/script.js
@@ -1,3 +1,31 @@
+/*Base de datos local*/
+const misCafes =[
+    {nombre: "Expreso", precio: 1.50, icono: "☕"},
+    {nombre: "Latte", precio: 2.50, icono:"🥛"},
+    {nombre: "Capuccino", precio: 3.00, icono:"🍫"},
+    {nombre: "Mocaccino", precio: 3.50, icono:"🧋"}
+];
+
+const contenedor = document.getElementById('contenedor-menu');
+
+/*Bucle para recorrer la lista de cafes*/
+
+misCafes.forEach(cafe => {
+    const estructuraCard = 
+                            `<div class="card-menu">
+                                <div class="icono">${cafe.icono}</div>
+                                    <h3>${cafe.nombre}</h3>
+                                        <p>Precio: ${cafe.precio.toFixed(2)}€</p>
+                                            <button class="btn-seleccionar" onclick="seleccionarCafe('${cafe.nombre}')">Seleccionar</button>
+                                            </div>`;
+                                            contenedor.innerHTML += estructuraCard;
+});
+
+/*Funcion para cuando se haga click en el menu*/
+
+function seleccionarCafe(nombre){
+    alert("Has elegido un " + nombre);
+}
 
 /*Boton Pedir Ahora*/
 const botonPedido = document.querySelector('.btn');
